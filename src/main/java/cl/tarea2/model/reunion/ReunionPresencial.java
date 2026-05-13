c817eb6f09e804390c8f910b0f1abaad0ddd639e
mguerrerozam <mguerrero2025@inf.udec.cl>
2026-05-13 17:23:36 -0400

    Feat: Añadir constructor a la clase `ReunionPresencial` para inicializar atributos

--- src/main/java/cl/tarea2/model/reunion/ReunionPresencial.java
+++ src/main/java/cl/tarea2/model/reunion/ReunionPresencial.java
@@ -1,5 +1,12 @@
 package cl.tarea2.model.reunion;
-
+import java.time.LocalDate;
+import java.time.LocalTime;
+import cl.tarea2.model.persona.Empleado;
 public class ReunionPresencial extends Reunion {
     private String sala;
+    public ReunionPresencial(TipoReunion tipoReunion, LocalDate fecha, LocalTime hora,
+                             int duracionPrevista, Empleado organizador, String sala) {
+        super(tipoReunion, fecha, hora, duracionPrevista, organizador);
+        this.sala = sala;
+    }
 }
