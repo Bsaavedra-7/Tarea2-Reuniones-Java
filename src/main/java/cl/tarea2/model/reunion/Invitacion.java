ad36f992949ca43db13cb29ac2a1e2b0eff74b2e
mguerrerozam <mguerrero2025@inf.udec.cl>
2026-05-13 17:17:18 -0400

    Feat: Añadir atributos `participante` y `hora` a la clase `Invitacion`

--- src/main/java/cl/tarea2/model/reunion/Invitacion.java
+++ src/main/java/cl/tarea2/model/reunion/Invitacion.java
@@ -1,4 +1,8 @@
 package cl.tarea2.model.reunion;
 
+import cl.tarea2.model.persona.Participante;
+import java.time.LocalTime;
 public class Invitacion {
+    private Participante participante;
+    private LocalTime hora;
 }
