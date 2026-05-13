8667c1e1dd97bc925578011f6457526216af8c96
mguerrerozam <mguerrero2025@inf.udec.cl>
2026-05-13 16:53:33 -0400

    Refactor: Actualizar atributos en clase `Reunion` para mayor detalle y organización

--- src/main/java/cl/tarea2/model/reunion/Reunion.java
+++ src/main/java/cl/tarea2/model/reunion/Reunion.java
@@ -3,11 +3,20 @@ package cl.tarea2.model.reunion;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
+
+import cl.tarea2.model.persona.Empleado;
 import cl.tarea2.model.persona.Participante;
+import cl.tarea2.util.Nota;
 
 public abstract class Reunion {
-    protected LocalDate fecha;
-    protected LocalTime hora;
-    protected int duracionPrevista;
-    protected List<Participante> invitados;
+    private TipoReunion tipoReunion;
+    private LocalDate fecha;
+    private LocalTime hora;
+    private LocalTime horaInicio;
+    private LocalTime horaFin;
+    private Empleado organizador;
+    private int duracionPrevista;
+    private List<Participante> invitados;
+    private List<Participante> asistentes;
+    private List<Nota> notas;
 }
