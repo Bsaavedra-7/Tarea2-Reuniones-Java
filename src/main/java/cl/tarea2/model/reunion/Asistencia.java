4f2c76988c50ac3e94761f4b4648bb97c961631b
mguerrerozam <mguerrero2025@inf.udec.cl>
2026-05-13 17:17:13 -0400

    Feat: Añadir nuevos atributos `participante` y `retraso` a la clase `Asistencia`

--- src/main/java/cl/tarea2/model/reunion/Asistencia.java
+++ src/main/java/cl/tarea2/model/reunion/Asistencia.java
@@ -1,4 +1,8 @@
 package cl.tarea2.model.reunion;
 
+import cl.tarea2.model.persona.Participante;
+
 public class Asistencia {
+    private Participante participante;
+    private Retraso retraso;
 }
