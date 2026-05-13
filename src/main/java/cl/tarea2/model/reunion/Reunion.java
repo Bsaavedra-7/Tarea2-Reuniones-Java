d4f71c87119de97e676fe71a0efeefae03163057
mguerrerozam <mguerrero2025@inf.udec.cl>
2026-05-13 17:23:27 -0400

    Feat: Añadir constructor a la clase `Reunion` para inicializar atributos

--- src/main/java/cl/tarea2/model/reunion/Reunion.java
+++ src/main/java/cl/tarea2/model/reunion/Reunion.java
@@ -3,6 +3,7 @@ package cl.tarea2.model.reunion;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
+import java.util.ArrayList;
 
 import cl.tarea2.model.persona.Empleado;
 import cl.tarea2.model.persona.Participante;
@@ -19,4 +20,15 @@ public abstract class Reunion {
     private List<Invitacion> invitados;
     private List<Asistencia> asistentes;
     private List<Nota> notas;
+
+    public Reunion(TipoReunion tipoReunion, LocalDate fecha, LocalTime hora, int duracionPrevista, Empleado organizador) {
+        this.tipoReunion = tipoReunion;
+        this.fecha = fecha;
+        this.hora = hora;
+        this.duracionPrevista = duracionPrevista;
+        this.organizador = organizador;
+        this.invitados = new ArrayList<>();
+        this.asistentes = new ArrayList<>();
+        this.notas = new ArrayList<>();
+    }
 }
