349dd784737733acc534cf6f3307bca5905b0328
Bastian Vásquez <bvasquez2025@udec.cl>
2026-05-14 04:44:08 -0400

    Agregar imports necesarios en la clase Informe

--- src/main/java/cl/tarea2/model/informe/Informe.java
+++ src/main/java/cl/tarea2/model/informe/Informe.java
@@ -1,5 +1,10 @@
 package cl.tarea2.model.informe;
 import cl.tarea2.model.persona.Empleado;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.io.IOException;
 
 public class informe {
 }
