5d018c8e24519f919aa72cb21fe08a1330196607
Bastian Vásquez <bvasquez2025@udec.cl>
2026-05-16 05:33:54 -0400

    Feat: Añadir a la clase Informe atributos tiempoReal, totalAsistencias y porcentajeAsistencias

--- src/main/java/cl/tarea2/model/informe/Informe.java
+++ src/main/java/cl/tarea2/model/informe/Informe.java
@@ -9,15 +9,28 @@ import cl.tarea2.model.reunion.*; //Importa todas las clases de reunion
 import java.util.List;
 
 public class Informe {
-    public Informe(Reunion ref, String nombreReunion) {//ref es la referencia a la reunion
-        //El informe debe proporcionar al menos:
+            //El informe debe proporcionar al menos:
 // Fecha y hora de la reunión
 // Horas de inicio y fin, y duración total
 // Información sobre el tipo de reunión
 // Enlace o sala
 // Lista de participantes y información sobre retrasos
 // Todas las notas relacionadas con la reunión (ordenadas
-//ronológicamente)
+//cronológicamente)
+
+    String tiempoReal;
+    String totalAsistencias;
+    String porcentajeAsistencias;
+
+
+
+
+    public Informe(Reunion ref, String nombreReunion) {//ref es la referencia a la reunion
+
+//-------------------------------------------------------------
+        //Los getters para el tiempo, el porcentaje y el total de asistencia no son String
+        //Por lo tanto hemos de convertirlos a String, sin embargo, este proceso puede ser "peligroso"
+        //Por ello, ha futuro hemos de considerar manjear excepciones aqui, es decir, usar un try
 
     }
 }
