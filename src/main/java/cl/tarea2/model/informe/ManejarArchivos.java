476a1bbfddf59053c8a4de1b84d01c00aab7eae5
Bastian Vásquez <bvasquez2025@udec.cl>
2026-05-17 22:11:57 -0400

    Crear clase ManejarArchivos

---
+++ src/main/java/cl/tarea2/model/informe/ManejarArchivos.java
@@ -0,0 +1,12 @@
+package cl.tarea2.model.informe;
+import cl.tarea2.model.persona.Empleado;
+import java.nio.file.*;
+import java.io.*;
+import cl.tarea2.model.reunion.*; //Importa todas las clases de reunion
+import java.util.List;
+
+public class ManejarArchivos{
+	public ManejarArchivos(){
+
+	}
+}
