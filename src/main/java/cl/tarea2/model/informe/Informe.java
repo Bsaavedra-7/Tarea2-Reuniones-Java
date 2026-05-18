5d7f81baeda761cba6aaf106cdcf46c683001fbc
Bastian Vásquez <bvasquez2025@udec.cl>
2026-05-17 23:37:54 -0400

    En  Informe crear  metodo reescribirInforme que deja el archivo vacío y vuelve a escribir la información

--- src/main/java/cl/tarea2/model/informe/Informe.java
+++ src/main/java/cl/tarea2/model/informe/Informe.java
@@ -69,4 +69,9 @@ public class Informe {
         archivoInforme.vaciarArchivo();
     }
 
+    public void reescribirInforme(){
+        archivoInforme.vaciarArchivo();
+        escribirInforme(); //Hacemos que el informe se escriba de nuevo
+    }
+
 }
