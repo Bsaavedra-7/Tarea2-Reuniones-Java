dd46b1c09b8d7c9f12665e4ad962f361359da29d
Bsaavedra <Bsaavedra2024@inf.udec.cl>
2026-05-18 17:58:30 -0400

    Actualize cambios en la clase Departamento

--- src/main/java/cl/tarea2/model/departamento/Departamento.java
+++ src/main/java/cl/tarea2/model/departamento/Departamento.java
@@ -1,9 +1,40 @@
 package cl.tarea2.model.departamento;
 
+import java.util.ArrayList;
 import java.util.List;
 import cl.tarea2.model.persona.Empleado;
 
 public class Departamento {
     private String nombre;
     private List<Empleado> empleados;
+
+    public Departamento(String nombre) {
+        this.nombre = nombre;
+        this.empleados = new ArrayList<>();
+    }
+
+    public void agregarEmpleado(Empleado e) {
+        empleados.add(e);
+    }
+
+    public List<Empleado> getEmpleados() {
+        return empleados;
+    }
+
+    public int getCantidadEmpleados() {
+        return empleados.size();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Departamento{nombre='" + nombre + "', cantidadEmpleados=" + getCantidadEmpleados() + '}';
+    }
 }
