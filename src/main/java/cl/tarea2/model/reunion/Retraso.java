c98dd263784b46ad52023b38ac1337b769ef36b0
mguerrerozam <mguerrero2025@inf.udec.cl>
2026-05-18 21:13:54 -0400

    Feat: Añadir constructor, getters, setters y `toString` a la clase `Retraso`

--- src/main/java/cl/tarea2/model/reunion/Retraso.java
+++ src/main/java/cl/tarea2/model/reunion/Retraso.java
@@ -2,4 +2,18 @@ package cl.tarea2.model.reunion;
 import java.time.LocalTime;
 public class Retraso {
     private LocalTime hora;
+
+    public Retraso(LocalTime hora) {
+        this.hora = hora;
+    }
+    public LocalTime getHora(){
+        return this.hora;
+    }
+    public void setHora(LocalTime hora){
+        this.hora = hora;
+    }
+    public String toString(){
+        return "Retraso de "+this.hora.toString();
+    }
+
 }
