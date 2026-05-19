df02ceba8959e91d7b998ca2ccfe2c8568d30cea
mguerrerozam <mguerrero2025@inf.udec.cl>
2026-05-18 21:16:43 -0400

    Feat: Añadir constructor, getters, setters y `toString` a la clase `Asistencia`

--- src/main/java/cl/tarea2/model/reunion/Asistencia.java
+++ src/main/java/cl/tarea2/model/reunion/Asistencia.java
@@ -5,4 +5,28 @@ import cl.tarea2.model.persona.Participante;
 public class Asistencia {
     private Participante participante;
     private Retraso retraso;
+    public Asistencia(Participante participante, Retraso retraso) {
+        this.participante = participante;
+        this.retraso = retraso;
+    }
+    public Participante getParticipante(){
+        return this.participante;
+    }
+    public Retraso getRetraso(){
+        return this.retraso;
+    }
+    public void setRetraso(Retraso retraso){
+        this.retraso = retraso;
+    }
+    public void setParticipante(Participante participante){
+        this.participante = participante;
+    }
+    public String toString(){
+        if(this.retraso == null) {
+            return "Asistencia de "+this.participante.getNombreCompleto();
+        }
+        else{
+        return "Asistencia de "+this.participante.getNombreCompleto()+" con retraso de "+this.retraso.getHora().toString();
+        }
+    }
 }
