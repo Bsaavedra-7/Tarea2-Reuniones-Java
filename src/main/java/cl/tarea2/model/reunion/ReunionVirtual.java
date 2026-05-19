997187bcd005710532bf30e7f7b4088d2bbc9947
mguerrerozam <mguerrero2025@inf.udec.cl>
2026-05-18 21:29:27 -0400

    Feat: Añadir getters, setters y `toString` a la clase `ReunionVirtual`

--- src/main/java/cl/tarea2/model/reunion/ReunionVirtual.java
+++ src/main/java/cl/tarea2/model/reunion/ReunionVirtual.java
@@ -5,8 +5,21 @@ import cl.tarea2.model.persona.Empleado;
 public class ReunionVirtual extends Reunion {
     private String enlace;
     public ReunionVirtual(TipoReunion tipoReunion, LocalDate fecha, LocalTime hora,
-                             int duracionPrevista, Empleado organizador, String sala) {
+                             int duracionPrevista, Empleado organizador, String enlace) {
         super(tipoReunion, fecha, hora, duracionPrevista, organizador);
         this.enlace = enlace;
+    }
+
+    public String getEnlace() {
+        return this.enlace;
+    }
+
+    public void setEnlace(String enlace) {
+        this.enlace = enlace;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nEnlace: " + this.enlace;
     }
 }
