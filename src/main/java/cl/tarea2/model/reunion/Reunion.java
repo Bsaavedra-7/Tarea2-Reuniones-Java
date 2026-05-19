2c34ca071fd9a4bc8bce25aa4fc5f66b392a158b
mguerrerozam <mguerrero2025@inf.udec.cl>
2026-05-18 21:28:11 -0400

    Feat: Añadir métodos para obtener asistentes, ausencias, retrasos, total y porcentaje de asistencia en `Reunion`

--- src/main/java/cl/tarea2/model/reunion/Reunion.java
+++ src/main/java/cl/tarea2/model/reunion/Reunion.java
@@ -60,4 +60,60 @@ public abstract class Reunion {
     public LocalTime obtenerHoraFin(){
         return this.horaFin;
     }
+
+    public List<Asistencia> obtenerAsistentes() {
+        return asistentes;
+    }
+
+    public List<Participante> obtenerAusencias() {
+        List<Participante> ausentes = new ArrayList<>();
+
+        for (Invitacion invitacion : invitados) {
+            Participante invitado = invitacion.getParticipante();
+            boolean estaPresente = false;
+
+            for (Asistencia asistencia : asistentes) {
+                if (asistencia.getParticipante().equals(invitado)) {
+                    estaPresente = true;
+                    break;
+                }
+            }
+
+            if (!estaPresente) {
+                ausentes.add(invitado);
+            }
+        }
+
+        return ausentes;
+    }
+
+    public List<Asistencia> obtenerRetrasos() {
+        List<Asistencia> asistenciasConRetraso = new ArrayList<>();
+
+        for (Asistencia asistencia : asistentes) {
+            if (asistencia.getRetraso() != null) {
+                asistenciasConRetraso.add(asistencia);
+            }
+        }
+
+        return asistenciasConRetraso;
+    }
+
+    public int obtenerTotalAsistencia() {
+        return asistentes.size();
+    }
+
+    public float obtenerPorcentajeAsistencia() {
+        return (float) asistentes.size() / (float) invitados.size() * 100;
+    }
+
+    @Override
+    public String toString() {
+        return "Reunión\n" +
+                "Fecha: " + fecha + "\n" +
+                "Hora: " + hora + "\n" +
+                "Tipo: " + tipoReunion + "\n" +
+                "Organizador: " + organizador.getNombreCompleto() + "\n" +
+                "Duración prevista: " + duracionPrevista + " minutos";
+    }
 }
