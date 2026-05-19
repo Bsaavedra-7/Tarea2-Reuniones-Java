be9b2b2356cc42840cae37c6814de809bec853ec
Bastian Vásquez <bvasquez2025@udec.cl>
2026-05-19 02:26:57 -0400

    Crear constructor y getter de Nota

--- src/main/java/cl/tarea2/util/Nota.java
+++ src/main/java/cl/tarea2/util/Nota.java
@@ -2,4 +2,12 @@ package cl.tarea2.util;
 
 public class Nota {
     private String contenido;
+
+    public Nota(String contenido){
+        this.contenido = contenido;
+    }
+
+    public String getContenido(){
+        return this.contenido;
+    }
 }
