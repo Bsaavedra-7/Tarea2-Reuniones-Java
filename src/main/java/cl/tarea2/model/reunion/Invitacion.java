b8d285a6437b225275af64f3ef2e7642fa9326aa
mguerrerozam <mguerrero2025@inf.udec.cl>
2026-05-18 21:12:28 -0400

    Feat: Añadir constructor, getters, setters y `toString` a la clase `Invitacion`

--- src/main/java/cl/tarea2/model/reunion/Invitacion.java
+++ src/main/java/cl/tarea2/model/reunion/Invitacion.java
@@ -5,4 +5,28 @@ import java.time.LocalTime;
 public class Invitacion {
     private Participante participante;
     private LocalTime hora;
+
+    public Invitacion(Participante participante, LocalTime hora) {
+        this.participante = participante;
+        this.hora = hora;
+    }
+    public Participante getParticipante(){
+        return this.participante;
+    }
+
+    public LocalTime getHora(){
+        return this.hora;
+    }
+
+    public void setHora(LocalTime hora){
+        this.hora = hora;
+    }
+
+    public void setParticipante(Participante participante){
+        this.participante = participante;
+    }
+
+    public String toString(){
+        return "Invitacion de "+this.participante.getNombreCompleto()+" a las "+this.hora.toString();
+    }
 }
